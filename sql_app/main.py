612c665b3f265208a96e715bfd1f512f561bf31d
jaehyeok3017 <gghh042404@gmail.com>
2022-12-27 01:32:40 +0900

    Fix : `main.py` email to id

--- sql_app/main.py
+++ sql_app/main.py
@@ -20,6 +20,7 @@ def get_db():
         db.close()
 
 
+
 @app.post("/users/", response_model=schemas.User)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_email(db, email=user.email)
@@ -34,7 +35,7 @@ def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     return users
 
 
-@app.get("/users/{user_email}", response_model=schemas.User)
+@app.get("/users/{user_id}", response_model=schemas.User)
 def read_user(user_id: int, db: Session = Depends(get_db)):
     db_user = crud.get_user(db, user_id=user_id)
     if db_user is None:
